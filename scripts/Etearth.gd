4d1c9866773d40c44954870e8a48339c98598145
trevormhighfill <129008272+trevormhighfill@users.noreply.github.com>
2024-03-03 16:53:09 -0600

    Worker update
    Workers will now collect the resources they are proficient in however as of right now the workers do not stack

--- scripts/Etearth.gd
+++ scripts/Etearth.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 class_name Etearth
 
 
-
+var unlocked_resources : Array[EnumHolder.natural_resources] = []
 @export var planet : PlanetResource
 @export var current_workers : Array[Workers] = []
 #@onready var main = $SpaceScript
@@ -10,32 +10,34 @@ class_name Etearth
 signal add_to_resource(amount : int,resource : EnumHolder.natural_resources)
 signal update_planet_info(current_workers:Array[Workers])
 
-func _process(delta):
-	pass
-	
-func _ready():
-	resource_gathering(planet.planet_natural_resources[0])
 
-func on_newly_added_worker(worker : Workers):
-	
-	pass
-	
 func resource_gathering(resource:NaturalResource):
 	await wait(resource.base_extraction_rate*planet.natural_resource_yield[0])
 	emit_signal("add_to_resource",resource.base_extraction_amount,resource)
-	resource_gathering(planet.planet_natural_resources[0])
+	resource_gathering(resource)
 	pass
 
 func wait(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
 
-
-#func _on_hire_hunter_button_down():
-	#current_workers.append()
-
-
-func _on_hire_hunter__newly_added_worker(worker):
+func _newly_added_worker(worker : Workers):
+	print(worker.proficient_resources.size())
+	for a in worker.proficient_resources.size():
+		var found : bool = false
+		if unlocked_resources.size() == 0:
+			unlocked_resources.append(worker.proficient_resources[a].natural_resource_type)
+			resource_gathering(worker.proficient_resources[a])
+			found = true
+		for b in unlocked_resources.size():
+			print(unlocked_resources[b])
+			if unlocked_resources[b] == worker.proficient_resources[a].natural_resource_type:
+				true
+		if found == false:
+			unlocked_resources.append(worker.proficient_resources[a].natural_resource_type)
+			resource_gathering(worker.proficient_resources[a])
+	
 	current_workers.append(worker)
 	emit_signal("update_planet_info",current_workers)
-	print("Hired ",current_workers[0].name, " and the employment team is ", current_workers.size())
+	print("Hired ",worker.name, " and the employment team is ", current_workers.size())
 	pass # Replace with function body.
+
