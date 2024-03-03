8665ad77d095ef4d8dda08ba573b9b1fca9d51ca
trevormhighfill <129008272+trevormhighfill@users.noreply.github.com>
2024-03-03 15:56:45 -0600

    Planet Toggle
    If you click on the selected planet it will now deselect and go back to the default planet information page (just says "Info")

--- scripts/EtearthButton.gd
+++ scripts/EtearthButton.gd
@@ -1,12 +1,20 @@
 extends Button
 
+signal deselected(currentworkers : Array[Workers])
 signal selected(currentworkers : Array[Workers])
 
 @onready var currentworkers : Array[Workers] = self.get_parent().current_workers
 
-func _ready():
-	var button = self#get_node("Hire Hunter")
-	button.pressed.connect(self._button_pressed.bind())
-	
-func _button_pressed():
-	emit_signal("selected",currentworkers)
+
+#func _button_pressed(presseddown : bool):
+	#if presseddown:
+		#emit_signal("selected",currentworkers)
+
+
+func _on_toggled(button_pressed):
+	if button_pressed:#.connect(self._button_pressed.bind(button.button_pressed))
+		emit_signal("selected",currentworkers)
+		print("toggled")
+	else:
+		emit_signal("deselected",currentworkers)
+		print("not toggled")
--- scripts/space.gd
+++ scripts/space.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 class_name SpaceScript
 
-@onready var popup = $Infobutton/ItemList
+
 var selected_planet : PlanetResource
 @export var planets: Array[PlanetResource] = []
 var resource_count = {
@@ -15,20 +15,23 @@ var resource_count = {
 	}
 
 func _on_infobutton_toggled(button_pressed):
+	var popup = $Infobutton/ItemList
 	if button_pressed == true:
 		popup.visible = true
 	else:
 		popup.visible = false
 
-func update_planet_info(currentworkers:Array[Workers]):
-	
+func update_planet_info(currentworkers):
+	var popup = $Infobutton/ItemList
 	#for i in popup.item_count:
 			#popup.remove_item(i)
 	popup.clear()
-	popup.add_item(selected_planet.planet_name)
-	for i in currentworkers.size():
-		popup.add_item(currentworkers[i].name)
-
+	if selected_planet != null or currentworkers != null:  
+		popup.add_item(selected_planet.planet_name)
+		for i in currentworkers.size():
+			popup.add_item(currentworkers[i].name)
+	else:
+		popup.add_item("Info")
 #func _on_etearth_button_button_down(currentworkers:Array[Workers]):
 	#selected_planet = planets[0]
 	#update_planet_info(currentworkers)
@@ -44,3 +47,9 @@ func _on_etearth_add_to_resource(amount,resource):
 func _on_etearth_button_selected(currentworkers):
 	selected_planet = planets[0]
 	update_planet_info(currentworkers)
+
+
+func button_deselected(currentworkers):
+	selected_planet = null
+	var empty = [null]
+	update_planet_info(null)
